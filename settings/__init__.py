7629a17214d25ceb0d1497eb7b85be9417f1c054
XinZeQiang <212338016@qq.com>
2018-11-06 15:05:19 +0800

    删除过时的设置项

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,7 +1,6 @@
-import os
 import datetime
-from library.core.common.simcardtype import CardType
-from library.core.common.supportedmodel import SupportedModel
+import os
+
 from .available_devices import AVAILABLE_DEVICES
 
 NOW = datetime.datetime.now()
@@ -18,19 +17,3 @@ REPORT_HTML_PATH = os.path.join(REPORT_PATH, 'TestReport.html')
 SCREEN_SHOT_PATH = os.path.join(REPORT_PATH, 'screen-shot', NOW.date().__str__())
 # log文件存放目录
 LOG_FILE_PATH = os.path.join(REPORT_PATH, 'log')
-
-# Appium 服务地址
-APPIUM_SETTING = {
-    "REMOTE_URL": "http://127.0.0.1:4723/wd/hub"
-}
-# Driver 默认的 desired_capabilities
-DEFAULT_DESIRED_CAPABILITY = {
-    "platformName": "Android",
-    "platformVersion": "8.0",
-    "deviceName": "test",
-    "udid": 'bb5671d',
-    "automationName": "UiAutomator2",
-    "newCommandTimeout": 600,
-    "appPackage": "com.chinasofti.rcs",
-    "appActivity": "com.cmcc.cmrcs.android.ui.activities.WelcomeActivity",
-}
