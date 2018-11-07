4b8b835ec2446211c55a981bac33def25f808f71
XinZeQiang <212338016@qq.com>
2018-11-07 19:59:55 +0800

    修复：Login用例0001（app版本6.2.5）

---
+++ pages/AgreementDetail.py
@@ -0,0 +1,32 @@
+from appium.webdriver.common.mobileby import MobileBy
+
+from library.core.BasePage import BasePage
+from library.core.TestLogger import TestLogger
+
+
+class AgreementDetailPage(BasePage):
+    """软件许可及服务协议-用户确认页面"""
+    ACTIVITY = 'com.cmcc.cmrcs.android.ui.activities.AgreementActivity'
+
+    __locators = {
+        # 'com.chinasofti.rcs:id/action_bar_root': (MobileBy.ID, 'com.chinasofti.rcs:id/action_bar_root'),
+        # 'android:id/content': (MobileBy.ID, 'android:id/content'),
+        # 'com.chinasofti.rcs:id/id_toolbar': (MobileBy.ID, 'com.chinasofti.rcs:id/id_toolbar'),
+        # 'com.chinasofti.rcs:id/back': (MobileBy.ID, 'com.chinasofti.rcs:id/back'),
+        '软件许可及服务协议': (MobileBy.ID, 'com.chinasofti.rcs:id/title'),
+        '和飞信软件许可及服务协议': (MobileBy.ID, ''),
+        '协议内容': (MobileBy.ID, 'com.chinasofti.rcs:id/tv_content2'),
+        '不同意': (MobileBy.ID, 'com.chinasofti.rcs:id/btn_cancel'),
+        '同意': (MobileBy.ID, 'com.chinasofti.rcs:id/btn_agree'),
+        # 'android:id/statusBarBackground': (MobileBy.ID, 'android:id/statusBarBackground')
+    }
+
+    @TestLogger.log()
+    def click_agree_button(self):
+        """点击同意"""
+        self.click_element(self.__locators['同意'])
+
+    @TestLogger.log()
+    def click_not_agree_button(self):
+        """点击不同意"""
+        self.click_element(self.__locators['不同意'])
