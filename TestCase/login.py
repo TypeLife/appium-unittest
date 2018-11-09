f933430ca5f7dfa2068fafbbf6606ed6744227b7
XinZeQiang <212338016@qq.com>
2018-11-09 10:21:54 +0800

    标记

--- TestCase/login.py
+++ TestCase/login.py
@@ -200,7 +200,7 @@ class LoginTest(TestCase):
     @unittest.skip("skip 本网单卡测试test_login_0006")
     def test_login_0006(self):
         """服务条款检查"""
-        # TODO 框架改动，待修复
+        # TODO XIN
         oklp = OneKeyLoginPage()
         # 点击许可服务协议
         oklp.click_license_agreement()
@@ -214,7 +214,7 @@ class LoginTest(TestCase):
     @unittest.skip("skip 本网单卡测试test_login_0007")
     def test_login_0007(self):
         """服务条款检查"""
-        # TODO 框架改动，待修复
+        # TODO XIN
         oklp = OneKeyLoginPage()
         # 点击许可服务协议
         oklp.click_license_agreement()
