495fbe602102efb65bab299bfb14e57bfa5f5d94
XinZeQiang <212338016@qq.com>
2018-11-14 19:59:44 +0800

    修改bug

--- mobileimplements/MXPro6Plus.py
+++ mobileimplements/MXPro6Plus.py
@@ -1,3 +1,6 @@
+from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.support.wait import WebDriverWait
+
 from library.core.common.simcardtype import CardType
 from library.core.mobile.mobiledriver import MobileDriver
 
@@ -13,3 +16,38 @@ class MXPro6Plus(MobileDriver):
 
     def total_card_slot(self):
         return 2
+
+    def wait_until(self, condition, timeout=8, auto_accept_permission_alert=True):
+        this = self
+
+        def execute_condition(driver):
+            """如果有弹窗，自动允许"""
+
+            def get_accept_permission_handler(d):
+                """获取允许权限弹窗的方法句柄"""
+                try:
+                    alert = d.switch_to.alert
+                    return alert.accept
+                except:
+                    alert = this.get_elements((MobileBy.XPATH, '//*[@text="始终允许" or @text="允许"]'))
+                    if not alert:
+                        return False
+                    return alert[0].click
+
+            if auto_accept_permission_alert:
+                # if this.driver.current_activity in [
+                #     'com.android.packageinstaller.permission.ui.GrantPermissionsActivity',
+                #     '.permission.ui.GrantPermissionsActivity'
+                # ]:
+                need = True
+                while need:
+                    try:
+                        WebDriverWait(this.driver, 1).until(
+                            get_accept_permission_handler
+                        )()
+                    except:
+                        need = False
+            return condition(driver)
+
+        wait = WebDriverWait(self.driver, timeout)
+        return wait.until(execute_condition)
