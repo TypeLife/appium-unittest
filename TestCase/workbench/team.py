5e6a896bdf52964e6738a24a26010e5945f62ec2
jlyuan <1120963865@qq.com>
2018-12-12 20:46:11 +0800

    添加创建团队相关操作

---
+++ TestCase/workbench/team.py
@@ -0,0 +1,16 @@
+import unittest
+from library.core.TestCase import TestCase
+
+
+class TeamTest(TestCase):
+    """团队 模块"""
+
+    def default_setUp(self):
+        pass
+
+    def default_tearDown(self):
+        pass
+
+    def test_something(self):
+        """description"""
+        self.assertEqual(True, True)
