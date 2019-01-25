1a9a003af11095491b7e07688aebf5d2c527a9b8
yangyuxin <401654459@qq.com>
2019-01-25 15:38:01 +0800

    修改自用手机配置信息

--- settings/available_devices.py
+++ settings/available_devices.py
@@ -322,7 +322,7 @@ DEVICES_CONFIG_XIN = {
 
 DEVICES_CONFIG_YYX = {
     'M960BDQN229CH': {
-        "MODEL": SupportedModel.MEIZU_PRO_6_PLUS,
+        "MODEL": SupportedModel.HUAWEI_P20,
         "SERVER_URL": 'http://127.0.0.1:4723/wd/hub',
         "DEFAULT_CAPABILITY": {
             "platformName": "Android",
