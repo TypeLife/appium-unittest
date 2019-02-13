c936c248e1b93cc72345f0176daa07245c373785
jlyuan <1120963865@qq.com>
2019-02-13 14:46:11 +0800

    优化模块前置，提取公共部分

--- TestCase/m002_message/msgcollection.py
+++ TestCase/m002_message/msgcollection.py
@@ -1,93 +1,12 @@
 from library.core.TestCase import TestCase
-from library.core.utils.applicationcache import current_mobile, switch_to_mobile
+from library.core.utils.applicationcache import current_mobile
+from preconditions.BasePreconditions import LoginPreconditions
 from library.core.utils.testcasefilter import tags
 from pages import *
-import time
-
-REQUIRED_MOBILES = {
-    # 'Android-移动': 'single_mobile',
-    'Android-移动': 'M960BDQN229CH',
-    'Android-移动-联通': 'mobile_and_union',
-    'Android-移动-电信': '',
-    'Android-移动-移动': 'double_mobile',
-    'Android-XX-XX': 'others_double',
-}
 
 
-class Preconditions(object):
+class Preconditions(LoginPreconditions):
     """前置条件"""
-
-    @staticmethod
-    def select_mobile(category, reset=False):
-        """选择手机"""
-        client = switch_to_mobile(REQUIRED_MOBILES[category])
-        client.connect_mobile()
-        if reset:
-            current_mobile().reset_app()
-        return client
-
-    @staticmethod
-    def make_already_in_one_key_login_page():
-        """已经进入一键登录页"""
-        # 如果当前页面已经是一键登录页，不做任何操作
-        one_key = OneKeyLoginPage()
-        if one_key.is_on_this_page():
-            return
-
-        # 如果当前页不是引导页第一页，重新启动app
-        guide_page = GuidePage()
-        if not guide_page.is_on_the_first_guide_page():
-            # current_mobile().launch_app()
-            current_mobile().reset_app()
-            guide_page.wait_for_page_load(20)
-
-        # 跳过引导页
-        guide_page.wait_for_page_load(30)
-        guide_page.swipe_to_the_second_banner()
-        guide_page.swipe_to_the_third_banner()
-        current_mobile().hide_keyboard_if_display()
-        guide_page.click_start_the_experience()
-
-        # 点击权限列表页面的确定按钮
-        permission_list = PermissionListPage()
-        permission_list.click_submit_button()
-        one_key.wait_for_page_load(30)
-
-    @staticmethod
-    def login_by_one_key_login():
-        """
-        从一键登录页面登录
-        :return:
-        """
-        # 等待号码加载完成后，点击一键登录
-        one_key = OneKeyLoginPage()
-        one_key.wait_for_page_load()
-        # one_key.wait_for_tell_number_load(60)
-        one_key.click_one_key_login()
-        if one_key.have_read_agreement_detail():
-            one_key.click_read_agreement_detail()
-            # 同意协议
-            agreement = AgreementDetailPage()
-            agreement.click_agree_button()
-
-        # 等待消息页
-        message_page = MessagePage()
-        message_page.wait_login_success(60)
-
-    @staticmethod
-    def make_already_in_message_page(reset=False):
-        """确保应用在消息页面"""
-        Preconditions.select_mobile('Android-移动', reset)
-        current_mobile().hide_keyboard_if_display()
-        time.sleep(1)
-        # 如果在消息页，不做任何操作
-        mess = MessagePage()
-        if mess.is_on_this_page():
-            return
-        # 进入一键登录页
-        Preconditions.make_already_in_one_key_login_page()
-        #  从一键登录页面登录
-        Preconditions.login_by_one_key_login()
 
     @staticmethod
     def init_and_enter_collection_page():
