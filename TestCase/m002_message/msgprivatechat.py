8bf0aa6149763ce0d9348cc6437077b5760b44b6
jlyuan <1120963865@qq.com>
2019-03-05 17:21:41 +0800

    add 0006~0008,0010

--- TestCase/m002_message/msgprivatechat.py
+++ TestCase/m002_message/msgprivatechat.py
@@ -29,7 +29,7 @@ class MsgPrivateChatMsgList(TestCase):
         pass
 
     def default_setUp(self):
-        """确保每个用例运行前在单聊会话页面"""
+        """确保每个用例运行前在消息页面"""
         Preconditions.select_mobile('Android-移动')
         mess = MessagePage()
         if mess.is_on_this_page():
@@ -105,3 +105,92 @@ class MsgPrivateChatMsgList(TestCase):
         scp.wait_for_create_msg_page_load()
         scp.click_back()
         mess.wait_for_page_load()
+
+    @tags('ALL', 'SMOKE', 'CMCC')
+    def test_Msg_PrivateChat_MsgList_0006(self):
+        """消息-消息列表界面新建消息页面返回操作"""
+        # 1、点击右上角的+号按钮
+        mess = MessagePage()
+        mess.click_add_icon()
+        mess.click_new_message()
+        # 2、点击手机系统内置返回返回按钮
+        scp = SelectContactsPage()
+        scp.wait_for_create_msg_page_load()
+        scp.driver.back()
+        mess.wait_for_page_load()
+
+    @tags('ALL', 'SMOKE', 'CMCC')
+    def test_Msg_PrivateChat_MsgList_0007(self):
+        """资料页发起聊天"""
+        # 1、在“联系人资料”页 点击【消息】,进入“个人会话”窗口
+        Preconditions.enter_private_chat_page()
+        # 返回消息列表页面
+        chat = SingleChatPage()
+        mess = MessagePage()
+        chat.click_back()
+        ContactDetailsPage().click_back()
+        mess.open_message_page()
+        mess.wait_for_page_load()
+
+    @staticmethod
+    def setUp_test_Msg_PrivateChat_MsgList_0008():
+        """消息列表点击消息记录前，先发送一条消息"""
+        Preconditions.enter_private_chat_page()
+        chat = SingleChatPage()
+        chat.input_message("hello")
+        chat.send_message()
+        chat.click_back()
+        ContactDetailsPage().click_back()
+        mess = MessagePage()
+        mess.open_message_page()
+        mess.wait_for_page_load()
+
+    @tags('ALL', 'SMOKE', 'CMCC')
+    def test_Msg_PrivateChat_MsgList_0008(self):
+        """消息-消息列表进入到会话页面"""
+        # 1、在消息列表点击消息记录，进入到会话页面
+        mess = MessagePage()
+        mess.click_msg_by_content("hello")
+        chat = SingleChatPage()
+        chat.wait_for_page_load()
+        chat.click_back()
+        mess.wait_for_page_load()
+
+    @staticmethod
+    def setUp_test_Msg_PrivateChat_MsgList_0010():
+        """消息列表点击消息记录前，先发送一条消息"""
+        Preconditions.enter_private_chat_page()
+        chat = SingleChatPage()
+        chat.input_message("hello")
+        chat.send_message()
+        chat.click_back()
+        ContactDetailsPage().click_back()
+        mess = MessagePage()
+        mess.open_message_page()
+        mess.wait_for_page_load()
+
+    @tags('ALL', 'SMOKE', 'CMCC')
+    def test_Msg_PrivateChat_MsgList_0010(self):
+        """消息-消息列表-消息列表显示未发送成功"""
+        # 1、 在会话页面输入文本消息
+        mess = MessagePage()
+        mess.click_msg_by_content("hello")
+        chat = SingleChatPage()
+        chat.wait_for_page_load()
+        # 断网
+        current_mobile().set_network_status(0)
+        chat.input_message("MsgList_0010")
+        # 2、点击发送
+        chat.send_message()
+        if not chat.is_msg_send_fail():
+            raise AssertionError("断网发送消息，在聊天会话窗无发送失败标志")
+        # 3、点击返回消息列表
+        chat.click_back()
+        mess.wait_for_page_load()
+        if not mess.is_iv_fail_status_present():
+            raise AssertionError("断网发送消息，在消息列表无发送失败标志")
+
+    @staticmethod
+    def tearDown_test_Msg_PrivateChat_MsgList_0010():
+        """恢复网络连接"""
+        current_mobile().set_network_status(6)
