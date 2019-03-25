bfa7c9d492e1c2449bde0d66a6f969787a114608
liuxiaodong <liu511759592@163.com>
2019-03-25 09:44:24 +0800

    优化部分用例代码

--- settings/available_devices.py
+++ settings/available_devices.py
@@ -407,7 +407,7 @@ DEVICES_CONFIG_LXD = {
         'CARDS': [
             {
                 'TYPE': CardType.CHINA_MOBILE,
-                'CARD_NUMBER': '15875537278'
+                'CARD_NUMBER': '19874361585'
             },
         ]
     },
