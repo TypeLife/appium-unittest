7f85105fcd613177e92ba33c869c2e5ff7909466
yangyuxin <401654459@qq.com>
2019-05-27 11:39:27 +0800

    优化建群

--- pages/SelectOneGroup.py
+++ pages/SelectOneGroup.py
@@ -251,3 +251,8 @@ class SelectOneGroupPage(BasePage):
             if name == t:
                 return True
         raise AssertionError('搜索结果"{}"没有找到与关键字"{}"完全匹配的文本'.format(texts, name))
+
+    @TestLogger.log()
+    def is_element_exit(self, text):
+        """指定元素是否存在"""
+        return self._is_element_present(self.__class__.__locators[text])
