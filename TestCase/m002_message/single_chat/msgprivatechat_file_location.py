95c5c71d3a2ea3c619592105608c67571803c6df
wanghailing <wanghailing@chinasoftinc.com>
2019-05-30 21:22:51 +0800

    编写消息模块用例2条msg_weifenglian_1V1_0092 0093

--- TestCase/m002_message/single_chat/msgprivatechat_file_location.py
+++ TestCase/m002_message/single_chat/msgprivatechat_file_location.py
@@ -3013,3 +3013,76 @@ class MsgPrivateChatAllTest(TestCase):
 
         mp = MessagePage()
         mp.set_network_status(6)
+
+    @tags('ALL', 'CMCC', 'LXD',  "lxd_debug")
+    def test_msg_weifenglian_1V1_0092(self):
+        """将自己发送的文件转发到手机联系人"""
+
+        scp = SingleChatPage()
+        # 等待单聊会话页面加载
+        scp.wait_for_page_load()
+        # 设置当前网络为2/3/4G
+        scp.set_network_status(4)
+        # 发送大型图片文件
+        Preconditions.send_large_picture_file()
+        time.sleep(2)
+        local_file = ChatSelectLocalFilePage()
+        # 1.是否弹出继续发送、订购免流特权、以后不再提示
+        self.assertEquals(local_file.is_exist_continue_send(), True)
+        self.assertEquals(local_file.is_exist_free_flow_privilege(), True)
+        self.assertEquals(local_file.is_exist_no_longer_prompt(), True)
+        time.sleep(2)
+        local_file.tap_coordinate([(100, 20), (100, 60), (100, 100)])
+        local_file.wait_for_page_load()
+        local_file.click_back()
+        csfp = ChatSelectFilePage()
+        csfp.click_back()
+        # 等待单聊会话页面加载
+        scp.wait_for_page_load()
+
+    @staticmethod
+    def tearDown_test_msg_weifenglian_1V1_0092():
+        """恢复网络"""
+
+        mp = MessagePage()
+        mp.set_network_status(6)
+
+    @tags('ALL', 'CMCC', 'LXD', "lxd_debug")
+    def test_msg_weifenglian_1V1_0093(self):
+        """将自己发送的文件转发到手机联系人时点击取消转发"""
+
+        scp = SingleChatPage()
+        # 等待单聊会话页面加载
+        scp.wait_for_page_load()
+        # 设置当前网络为2/3/4G
+        scp.set_network_status(4)
+        # 发送大型图片文件
+        Preconditions.send_large_picture_file()
+        local_file = ChatSelectLocalFilePage()
+        # 点击继续发送
+        local_file.click_continue_send()
+        # 1.验证是否发送成功
+        cwp = ChatWindowPage()
+        cwp.wait_for_msg_send_status_become_to('发送成功', 30)
+        # 再次选择大型图片文件发送
+        Preconditions.send_large_picture_file()
+        time.sleep(2)
+        # 2.是否弹出继续发送、订购免流特权、以后不再提示
+        self.assertEquals(local_file.is_exist_continue_send(), True)
+        self.assertEquals(local_file.is_exist_free_flow_privilege(), True)
+        self.assertEquals(local_file.is_exist_no_longer_prompt(), True)
+        time.sleep(2)
+        local_file.tap_coordinate([(100, 20), (100, 60), (100, 100)])
+        local_file.wait_for_page_load()
+        local_file.click_back()
+        csfp = ChatSelectFilePage()
+        csfp.click_back()
+        # 等待单聊会话页面加载
+        scp.wait_for_page_load()
+
+    @staticmethod
+    def tearDown_test_msg_weifenglian_1V1_0093():
+        """恢复网络"""
+
+        mp = MessagePage()
+        mp.set_network_status(6)
