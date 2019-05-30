89dcbea5a1135ce61ae8694cdfd59c8a36df7793
xiongshan <xiongshan@chinasoftinc.com>
2019-05-30 10:14:20 +0800

    新添工作台公告信息用例GGXX_003,GGXX_004，GGXX_006

--- TestCase/m004_workbench/announcement_message.py
+++ TestCase/m004_workbench/announcement_message.py
@@ -76,7 +76,74 @@ class AnnouncementMessageTest(TestCase):
         wbp = WorkbenchPage()
         wbp.wait_for_page_load()
 
+    @tags('ALL', 'CMCC', 'workbench', 'GGXX')
+    def test_GGXX_0003(self):
+        """检查点击关闭按钮控件【X】"""
+        # 1、管理员登录移动端和飞信工作台
+        # 2、点击【x】
+        amp = AnnouncementMessagePage()
+        amp.wait_for_page_loads()
+        time.sleep(3)
+        amp.click_text("发布公告")
+        time.sleep(2)
+        amp.click_element_("X")
+        wbp = WorkbenchPage()
+        wbp.wait_for_page_load()
 
+    @tags('ALL', 'CMCC', 'workbench', 'GGXX')
+    def test_GGXX_0004(self):
+        """管理员进入发布公告，检查初始化页面"""
+        # 1、管理员登录移动端和飞信工作台
+        # 2、点击进入【公告信息】初始化页面
+        # 3、检查【公告信息】初始化页面
+        amp = AnnouncementMessagePage()
+        amp.wait_for_page_loads()
+        time.sleep(2)
+        if not amp.is_text_present("向团队所有成员发出第一条公告"):
+            raise AssertionError("没有出现初始化页面显示文字")
+        if not amp.is_text_present("发布公告"):
+            raise AssertionError("没有发布公告按钮")
+        if not amp.is_text_present("未发公告"):
+            raise AssertionError("没有未发公告按钮")
+        time.sleep(2)
+        current_mobile().back()
+        wbp = WorkbenchPage()
+        wbp.wait_for_page_load()
+
+    @tags('ALL', 'CMCC', 'workbench', 'GGXX')
+    def test_GGXX_0006(self):
+        """管理员进入发布公告，公告搜索-按中文搜索"""
+        # 1、管理员登录移动端和飞信工作台
+        # 2、点击进入【公告信息】页面
+        # 3、按中文搜索公告信息
+        amp = AnnouncementMessagePage()
+        amp.wait_for_page_loads()
+        amp.click_text("发布公告")
+        time.sleep(2)
+        amp.input_title_text("哈哈")
+        amp.input_content_text("你好啊")
+        current_mobile().hide_keyboard()
+        time.sleep(2)
+        amp.click_element_("发布")
+        time.sleep(2)
+        amp.click_element_("确定")
+        amp.wait_for_page_loads()
+        amp.click_element_("搜索")
+        time.sleep(2)
+        amp.click_element_("搜索输入框")
+        time.sleep(2)
+        amp.input_search_text("哈")
+        time.sleep(3)
+        amp.click_text("搜索")
+        time.sleep(2)
+        if not amp.is_text_present("哈哈"):
+            raise AssertionError("搜索不成功")
+        amp.click_text("哈哈")
+        time.sleep(2)
+        amp.click_text("下线")
+        time.sleep(2)
+        amp.click_element_("确定")
+        amp.wait_for_page_loads()
 
 
 
