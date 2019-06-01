b7684fd9da5890368bd56e3c0431828f623d91cf
jiangxue002 <jiangxue002@chinasoftinc.com>
2019-06-01 18:39:53 +0800

    call_wangqiong_0146、147、155、157、171

--- TestCase/m007_others/other_call/call.py
+++ TestCase/m007_others/other_call/call.py
@@ -1,8 +1,10 @@
 import time
 from appium.webdriver.common.mobileby import MobileBy
 from library.core.utils.applicationcache import current_mobile
+from pages.components import ContactsSelector
 from preconditions.BasePreconditions import LoginPreconditions, ContactsPage, CallPage, ContactSecltorPage, \
-    SelectContactsPage, CalllogBannerPage, MessagePage, SearchPage, LabelGroupingPage
+    SelectContactsPage, CalllogBannerPage, MessagePage, SearchPage, LabelGroupingPage, GroupListPage, \
+    GroupListSearchPage
 from library.core.TestCase import TestCase
 from library.core.utils.testcasefilter import tags
 
@@ -745,4 +747,293 @@ class MsgAllPrior(TestCase):
         call_page._is_element_present((MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/tvCallManner" and ' +
                                        '@text="和飞信电话"]'))
         time.sleep(3)
+
+    @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
+    def test_call_call_wangqiong_0146(self):
+        # 启动App
+        Preconditions.select_mobile('Android-移动')
+        # 启动后不论当前在哪个页面，强制进入消息页面
+        Preconditions.force_enter_message_page('Android-移动')
+        # 下面根据用例情况进入相应的页面
+        """需要预置联系人"""
+        contactspage = ContactsPage()
+        contactspage.open_contacts_page()
+        contactspage.create_contacts_if_not_exits('联系人1', '18312345678')
+        contactspage.create_contacts_if_not_exits('联系人2', '18323456789')
+        contactspage.create_contacts_if_not_exits('联系人3', '13812345678')
+        contactspage.create_contacts_if_not_exits('联系人4', '13823456789')
+        contactspage.create_contacts_if_not_exits('联系人5', '13811111111')
+        contactspage.create_contacts_if_not_exits('联系人6', '13822222222')
+        contactspage.create_contacts_if_not_exits('联系人7', '13833333333')
+        contactspage.create_contacts_if_not_exits('联系人8', '13844444444')
+
+        Preconditions.enter_call_page()
+        # 点击多方通话
+        call_page = CallPage()
+        call_page.click_free_call()
+        # 进入多方通话页面选择联系人呼叫
+        selectcontacts = SelectContactsPage()
+        selectcontacts.search('联系人1')
+        selectcontacts.click_element(
+            (MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/contact_name" and ' +
+             '@text="联系人1"]'))
+        selectcontacts.search('联系人2')
+        selectcontacts.click_element(
+            (MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/contact_name" and ' +
+             '@text="联系人2"]'))
+        selectcontacts.search('联系人3')
+        selectcontacts.click_element(
+            (MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/contact_name" and ' +
+             '@text="联系人3"]'))
+        selectcontacts.search('联系人4')
+        selectcontacts.click_element(
+            (MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/contact_name" and ' +
+             '@text="联系人4"]'))
+        selectcontacts.search('联系人5')
+        selectcontacts.click_element(
+            (MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/contact_name" and ' +
+             '@text="联系人5"]'))
+        selectcontacts.search('联系人6')
+        selectcontacts.click_element(
+            (MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/contact_name" and ' +
+             '@text="联系人6"]'))
+        selectcontacts.search('联系人7')
+        selectcontacts.click_element(
+            (MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/contact_name" and ' +
+             '@text="联系人7"]'))
+        selectcontacts.search('联系人8')
+        selectcontacts.click_element(
+            (MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/contact_name" and ' +
+             '@text="联系人8"]'))
+        time.sleep(5)
+        selectcontacts.click_sure_bottom()
+        # 是否弹框_我知道了,点击 发起呼叫
+        callcontact = CalllogBannerPage()
+        # 是否存在请先接听“和飞信电话”，点击“我知道了”
+        if callcontact._is_element_present((MobileBy.ID, 'com.chinasofti.rcs:id/andfetion_tip_bt')):
+            callcontact.click_element((MobileBy.ID, 'com.chinasofti.rcs:id/andfetion_tip_bt'))
+        # 是否存在设置悬浮窗，存在去设置页设置权限
+        if callcontact._is_element_present((MobileBy.ID, 'android:id/button1')):
+            callcontact.click_element((MobileBy.ID, 'android:id/button1'))
+            current_mobile().click_element((MobileBy.ID, 'android:id/switch_widget'))
+            current_mobile().click_element((MobileBy.XPATH, '//android.widget.ImageButton[@content-desc="向上导航"]'))
+
+        # 挂断多方通话
+        time.sleep(3)
+        call_page.hang_up_hefeixin_call()
+        time.sleep(3)
+        # 查看通话类型为和飞信通话
+        call_page._is_element_present((MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/tvCallTime" and ' +
+                                       '@text="刚刚"]'))
+        call_page._is_element_present((MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/tvCallManner" and ' +
+                                       '@text="和飞信电话"]'))
+        time.sleep(3)
+
+    @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
+    def test_call_call_wangqiong_0147(self):
+        # 启动App
+        Preconditions.select_mobile('Android-移动')
+        # 启动后不论当前在哪个页面，强制进入消息页面
+        Preconditions.force_enter_message_page('Android-移动')
+        # 下面根据用例情况进入相应的页面
+        """需要预置联系人"""
+        contactspage = ContactsPage()
+        contactspage.open_contacts_page()
+        contactspage.create_contacts_if_not_exits('联系人1', '18312345678')
+
+        Preconditions.enter_call_page()
+        # 点击多方通话
+        call_page = CallPage()
+        call_page.click_free_call()
+        time.sleep(4)
+        # 进入多方通话页面选择联系人呼叫
+        selectcontacts = SelectContactsPage()
+        selectcontacts.search('联系人1')
+        selectcontacts.click_element(
+            (MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/contact_name" and ' +
+             '@text="联系人1"]'))
+        selectcontacts.search('18311111111')
+        selectcontacts.click_unknown_member()
+        selectcontacts.search('18322222222')
+        selectcontacts.click_unknown_member()
+        selectcontacts.search('18333333333')
+        selectcontacts.click_unknown_member()
+        selectcontacts.search('18333333333')
+        selectcontacts.click_unknown_member()
+        selectcontacts.search('18344444444')
+        selectcontacts.click_unknown_member()
+        selectcontacts.search('18355555555')
+        selectcontacts.click_unknown_member()
+        selectcontacts.search('18366666666')
+        selectcontacts.click_unknown_member()
+
+        time.sleep(5)
+        selectcontacts.click_sure_bottom()
+        # 是否弹框_我知道了,点击 发起呼叫
+        callcontact = CalllogBannerPage()
+        # 是否存在请先接听“和飞信电话”，点击“我知道了”
+        if callcontact._is_element_present((MobileBy.ID, 'com.chinasofti.rcs:id/andfetion_tip_bt')):
+            callcontact.click_element((MobileBy.ID, 'com.chinasofti.rcs:id/andfetion_tip_bt'))
+        # 是否存在设置悬浮窗，存在去设置页设置权限
+        if callcontact._is_element_present((MobileBy.ID, 'android:id/button1')):
+            callcontact.click_element((MobileBy.ID, 'android:id/button1'))
+            current_mobile().click_element((MobileBy.ID, 'android:id/switch_widget'))
+            current_mobile().click_element((MobileBy.XPATH, '//android.widget.ImageButton[@content-desc="向上导航"]'))
+
+        # 挂断多方通话
+        time.sleep(4)
+        call_page.hang_up_hefeixin_call()
+        time.sleep(3)
+        # 查看通话类型为和飞信通话
+        call_page._is_element_present((MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/tvCallTime" and ' +
+                                       '@text="刚刚"]'))
+        call_page._is_element_present((MobileBy.XPATH, '//*[@resource-id="com.chinasofti.rcs:id/tvCallManner" and ' +
+                                       '@text="和飞信电话"]'))
+        time.sleep(3)
+
+    @staticmethod
+    def setUp_test_call_wangqiong_0155():
+        """预置条件"""
+        # 启动App
+        Preconditions.select_mobile('Android-移动')
+        # 启动后不论当前在哪个页面，强制进入消息页面
+        Preconditions.force_enter_message_page('Android-移动')
+        # 下面根据用例情况进入相应的页面
+        """需要预置联系人"""
+        contactspage = ContactsPage()
+        contactspage.open_contacts_page()
+        contactspage.create_contacts_if_not_exits('联系人3', '18311111111')
+        contactspage.create_contacts_if_not_exits('联系人4', '18322222222')
+        # 进入群聊分组页签
+        contactspage.open_group_chat_list()
+
+    @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
+    def test_call_wangqiong_0155(self):
+        # 创建标签分组
+        grouppage = GroupListPage()
+        # 创建群聊1
+        grouppage.create_group_chats_if_not_exits('群聊1', ["联系人3", "联系人4"])
+        # 进入群聊的多方电话
+        grouppage.click_group('群聊1')
+        grouppage.click_mult_call_icon()
+        time.sleep(3)
+        grouppage.click_element((MobileBy.XPATH, "//*[@text='多方电话']"))
+
+        # 选择成员进行多方电话
+
+        time.sleep(10)
+
+    @staticmethod
+    def setUp_test_call_wangqiong_0157():
+        """预置条件"""
+        # 启动App
+        Preconditions.select_mobile('Android-移动')
+        # 启动后不论当前在哪个页面，强制进入消息页面
+        Preconditions.force_enter_message_page('Android-移动')
+        # 下面根据用例情况进入相应的页面
+        """需要预置联系人"""
+        contactspage = ContactsPage()
+        contactspage.open_contacts_page()
+        contactspage.create_contacts_if_not_exits('联系人3', '18311111111')
+        contactspage.create_contacts_if_not_exits('联系人4', '18322222222')
+        # 进入群聊分组页签
+        contactspage.open_group_chat_list()
+
+    @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
+    def test_call_wangqiong_0157(self):
+        # 创建标签分组
+        grouppage = GroupListPage()
+        # 创建群聊1
+        grouppage.create_group_chats_if_not_exits('群聊1', ["联系人3", "联系人4"])
+        # 进入群聊的多方电话
+        grouppage.click_search_input()
+        groupserch = GroupListSearchPage()
+        groupserch.input_search_keyword('群聊1')
+        # 点击群聊1
+        groupserch.click_group('群聊1')
+        grouppage.click_mult_call_icon()
+        time.sleep(3)
+        grouppage.click_element((MobileBy.XPATH, "//*[@text='多方电话']"))
+
+        contactselect = ContactsSelector()
+        # 选择自己进行多方电话,弹框该联系人不可选择,呼叫按钮任然置灰
+        contactselect.click_element((MobileBy.ID, 'com.chinasofti.rcs:id/index_text'))
+        current_mobile().assert_element_should_be_disabled((MobileBy.ID, 'com.chinasofti.rcs:id/tv_sure'))
+
+    @staticmethod
+    def setUp_test_call_wangqiong_0171():
+        """预置条件"""
+        # 启动App
+        Preconditions.select_mobile('Android-移动')
+        # 启动后不论当前在哪个页面，强制进入消息页面
+        Preconditions.force_enter_message_page('Android-移动')
+        # 下面根据用例情况进入相应的页面
+        """需要预置联系人"""
+        contactspage = ContactsPage()
+        contactspage.open_contacts_page()
+        # contactspage.create_contacts_if_not_exits('A联系人', '18311111111')
+        # contactspage.create_contacts_if_not_exits('B联系人', '18322222222')
+        # contactspage.create_contacts_if_not_exits('C联系人', '18333333333')
+        # contactspage.create_contacts_if_not_exits('D联系人', '18344444444')
+        # contactspage.create_contacts_if_not_exits('E联系人', '18355555555')
+        # contactspage.create_contacts_if_not_exits('F联系人', '18366666666')
+        # contactspage.create_contacts_if_not_exits('G联系人', '18377777777')
+        # contactspage.create_contacts_if_not_exits('H联系人', '18388888888')
+        # contactspage.create_contacts_if_not_exits('I联系人', '18399999999')
+        # contactspage.create_contacts_if_not_exits('J联系人', '13822222222')
+        # contactspage.create_contacts_if_not_exits('K联系人', '13811111111')
+        # 进入标签分组页签
+        contactspage.click_label_grouping()
+
+    @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
+    def test_call_wangqiong_0171(self):
+        # 创建标签分组
+        labellist = LabelGroupingPage()
+        labellist.click_new_create_group()
+        labellist.wait_for_create_label_grouping_page_load()
+        labellist.input_label_grouping_name('分组A')
+        labellist.click_sure()
+        time.sleep(3)
+        if current_mobile().is_text_present('新建分组'):
+            labellist.click_element((MobileBy.ID, 'com.chinasofti.rcs:id/rl_label_left_back'))
+            labellist.select_group('分组A')
+
+            # 判断标签中有无指定成员
+            if labellist._is_element_present((MobileBy.ID, 'com.chinasofti.rcs:id/dialog_message')):
+                time.sleep(5)
+                labellist.click_element((MobileBy.ID, 'com.chinasofti.rcs:id/btn_cancel'),
+                                        auto_accept_permission_alert=False)
+            a = labellist.is_contacter_in_lable('联系人3')
+            b = labellist.is_contacter_in_lable('联系人4')
+            if not (a & b):
+                labellist.click_element((MobileBy.ID, 'com.chinasofti.rcs:id/image_first_colum'),
+                                        auto_accept_permission_alert=False)
+                if not a:
+                    labellist.click_one_contact('联系人3')
+                if not b:
+                    labellist.click_one_contact('联系人4')
+                labellist.click_sure()
+
+        # 创建标签分组
+        labellist = LabelGroupingPage()
+        labellist.create_group('分组A', ["A联系人", "B联系人", "C联系人"])
+        # labellist.create_group('分组A', ['A联系人', 'B联系人', 'C联系人', 'D联系人', 'E联系人', 'F联系人', 'G联系人', 'H联系人', 'I联系人', 'J联系人', 'K联系人'])
+        labellist.click_label_group('分组A')
+        # 进入多方通话
+        labellist.click_element((MobileBy.ID, 'com.chinasofti.rcs:id/image_third_colum'))
+        # 查看时长
+        labellist.click_element((MobileBy.ID, 'com.chinasofti.rcs: id/multi_time_tip'))
+        self.assertTrue(labellist.is_text_present((MobileBy.XPATH, '//*[contains(@text, "分钟")]')))
+
+        # 按照字母滑动
+        contact = ContactsPage()
+        contact.click_element(
+            ('xpath', '//*[@resource-id="com.chinasofti.rcs:id/indexbarview"]'))
+        elements = contact.get_elements((MobileBy.ID, 'com.chinasofti.rcs:id/tv_name'))
+        for i in range(len(elements)):
+            elements[i].click()
+        # 判断右侧字符是否按顺序排列
+        current_mobile().is_right_letters_sorted()
+
+
 
