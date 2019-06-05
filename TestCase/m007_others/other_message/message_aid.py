6c5cdfc77ebae30e655ee5721edc6016bab3f31a
liwenxiu <liwenxiu01@chinasoftinc.com>
2019-06-05 18:25:47 +0800

    消息模块

--- TestCase/m007_others/other_message/message_aid.py
+++ TestCase/m007_others/other_message/message_aid.py
@@ -2579,7 +2579,6 @@ class MsgAllPrior(TestCase):
         # 下面根据用例情况进入相应的页面
         Preconditions.create_contacts_if_not_exist(["测试短信1, 13800138111", "测试短信2, 13800138112"])
 
-    liwenxiu
     @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
     def test_msg_xiaoqiu_0195(self):
         """群聊设置页面——查找聊天内容"""
