afc025044dea74e6465f83f57496e3620a9af7ef
lili <lili011@chinasoftinc.com>
2019-06-10 21:39:46 +0800

    编写通话模块用例1条 call_wangqiong_0494 编写消息模块用例1条 msg_huangcaizui_A_0091

--- TestCase/m007_others/other_workbench/workbench_all.py
+++ TestCase/m007_others/other_workbench/workbench_all.py
@@ -1837,7 +1837,63 @@ class MsgAllPrior(TestCase):
         # checkpoint: 刚才拨打的类型为【电话】,号码包含12560
         callpage.is_type_hefeixin(0, '电话')
 
+    @staticmethod
+    def setUp_test_call_wangqiong_0494():
+        """预置条件"""
 
+    @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
+    def test_call_wangqiong_0494(self):
+        """成功创建会场成功后（12560回呼接通）挂断当前通话"""
+        # 1,发起呼叫和飞信，成功创建会场时，进入会控页，点击挂断按钮
+
+        # 启动App
+        Preconditions.select_mobile('Android-移动')
+        # 启动后不论当前在哪个页面，强制进入消息页面
+        Preconditions.force_enter_message_page_631()
+        # 下面根据用例情况进入相应的页面
+        # 需要预置联系人
+        contactname1 = Preconditions.contacts_name_1
+        contactnum1 = Preconditions.telephone_num_1
+        contactname2 = Preconditions.contacts_name_2
+        contactnum2 = Preconditions.telephone_num_2
+        # 新建联系人
+        contactspage = ContactsPage()
+        contactspage.open_contacts_page()
+        contactspage.create_contacts_if_not_exits_631(contactname1, contactnum1)
+        contactspage.create_contacts_if_not_exits_631(contactname2, contactnum2)
+
+        Preconditions.enter_call_page()
+        # 如果存在多方通话引导页跳过引导页
+        callcontact = CalllogBannerPage()
+        callcontact.skip_multiparty_call()
+        # 点击多方通话
+        callcontact.click_free_call()
+        # 选择指定联系人 点击呼叫
+        selectcontacts = SelectContactsPage()
+        selectcontacts.search(contactname1)
+        selectcontacts.click_contact_by_name(contactname1)
+        time.sleep(4)
+        selectcontacts.click_sure_bottom()
+        time.sleep(1)
+
+        # 是否存在请先接听“和飞信电话”，点击“我知道了” 并自动允许和飞信管理
+        callcontact.click_elsfif_ikonw()
+
+        # 是否存在权限窗口 自动赋权
+        from pages import GrantPemissionsPage
+        grantpemiss = GrantPemissionsPage()
+        grantpemiss.allow_contacts_permission()
+
+        # 是否存在设置悬浮窗，存在暂不开启
+        from pages.components.dialogs import SuspendedTips
+        suspend = SuspendedTips()
+        suspend.ignore_tips_if_tips_display()
+        # 挂断多方通话
+        callpage = CallPage()
+        callpage.hang_up_hefeixin_call_631()
+        time.sleep(3)
+        # Checkpoint：挂断电话回到多方通话界面
+        self.assertTrue(callpage.is_on_the_call_page())
 
 
 class Contacts_demo(TestCase):
@@ -4093,3 +4149,30 @@ class Contacts_demo(TestCase):
         GroupChatPage().click_setting()
         GroupChatSetPage().wait_for_page_load()
         GroupChatSetPage().click_delete_and_exit()
+
+    @staticmethod
+    def setUp_test_msg_huangcaizui_A_0091():
+        # 启动App
+        Preconditions.select_mobile('Android-移动')
+        # 启动后不论当前在哪个页面，强制进入消息页面
+        Preconditions.force_enter_message_page('Android-移动')
+        # 下面根据用例情况进入相应的页面
+        Preconditions.create_contacts_if_not_exist_631(["测试短信1, 13800138111", "测试短信2, 13800138112"])
+
+    @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
+    def test_msg_huangcaizui_A_0091(self):
+        """一对一聊天设置创建群聊"""
+        # 1.已有一对一的聊天窗口
+        mess = MessagePage()
+        singlechat = SingleChatPage()
+        # Step 1.进入一对一聊天窗口
+        mess.search_and_enter_631('测试短信1')
+        ContactDetailsPage().click_message_icon()
+        singlechat.wait_for_page_load()
+        # Step 2.点击进入聊天设置，再点击+添加成员
+        singlechat.click_setting()
+        SingleChatSetPage().click_add_icon()
+        # Step 3.选择一个或多个成员,点击确定
+        ContactsSelector().select_local_contacts('测试短信2')
+        # Checkpoint 1.进入群聊名称设置
+        GroupNamePage().wait_for_page_load_631()
