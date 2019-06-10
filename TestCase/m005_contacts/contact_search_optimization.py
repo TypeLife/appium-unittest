38a83dcaa29b95131c8a9864ac831a9de364caeb
yaolei <yaolei002@chinasoftinc.com>
2019-06-10 11:55:43 +0800

    contacts_chenjixiang_0741 contacts_chenjixiang_0742

--- TestCase/m005_contacts/contact_search_optimization.py
+++ TestCase/m005_contacts/contact_search_optimization.py
@@ -5,7 +5,7 @@ from library.core.utils.applicationcache import current_mobile
 from library.core.utils.testcasefilter import tags
 from pages import *
 from preconditions.BasePreconditions import LoginPreconditions
-
+from pages.workbench.group_messenger.SelectCompanyContacts import SelectCompanyContactsPage
 
 class Preconditions(LoginPreconditions):
     """前置条件"""
@@ -51,9 +51,9 @@ class ContactSearchOpTest(TestCase):
                         conts.click_text("显示")
                 except:
                     pass
-                # for name, number in required_contacts:
-                #     # 创建联系人
-                #     conts.create_contacts_if_not_exits(name, number)
+                for name, number in required_contacts:
+                    # 创建联系人
+                    conts.create_contacts_if_not_exits(name, number)
                 required_group_chats = dataproviders.get_preset_group_contacts()
                 conts.open_group_chat_list()
                 group_list = GroupListPage()
@@ -89,7 +89,132 @@ class ContactSearchOpTest(TestCase):
     def default_tearDown(self):
         pass
 
+    @tags('ALL', 'CONTACT','YL')
+    def test_contacts_quxinli_0010(self):
+        # 导入团队联系人
+        fail_time2 = 0
+        flag2 = False
+        while fail_time2 < 5:
+            try:
+                Preconditions.make_already_in_message_page()
+                contact_names = ["大佬1", "大佬2", "大佬3", "大佬4"]
+                Preconditions.create_he_contacts(contact_names)
+                contact_names2 = [("高亮1", "18826200000")]
+                Preconditions.create_he_contacts2(contact_names2)
+                flag2 = True
+            except:
+                fail_time2 += 1
+            if flag2:
+                break
+
+        Preconditions.make_already_in_message_page()
+        # 点击‘通讯录’
+        mess = MessagePage()
+        mess.open_contacts_page()
+        # 1、点击通讯录，点击搜索输入框
+        # 2、输入特殊字符
+        mess.click_search()
+        # 查询页面输入'大佬1'
+        search_page = SearchPage()
+        search_number = "188262"
+        search_page.input_search_keyword(search_number)
+        time.sleep(5)
+        search_page.hide_keyboard()
+        # 判定点
+        # 1、自动匹配输入结果，搜索内容高亮  准自动化
+        # 1.检查搜索结果是否模糊匹配关键字
+        sccp = SelectCompanyContactsPage()
+        self.assertEquals(sccp.is_search_contacts_number_match(search_number), True)
+
+    @tags('ALL', 'CONTACT', 'YL')
+    def test_contacts_chenjixiang_0736(self):
+        Preconditions.make_already_in_message_page()
+        # 点击‘通讯录’
+        mess = MessagePage()
+        mess.open_contacts_page()
+        # 1、点击通讯录，点击搜索输入框
+        # 2、输入特殊字符
+        mess.click_search()
+        # 查询页面输入'+'
+        search_page = SearchPage()
+        search_page.input_search_keyword('特殊!@$')
+        time.sleep(5)
+        search_page.hide_keyboard()
+        # 判定点
+        # 1.模糊匹配到正确的结果
+        self.assertEquals(search_page.is_text_present("特殊!@$"), False)
+
     @tags('ALL', 'SMOKE')
+    def test_contacts_chenjixiang_0739(self):
+        # 导入团队联系人
+        fail_time2 = 0
+        flag2 = False
+        while fail_time2 < 5:
+            try:
+                Preconditions.make_already_in_message_page()
+                contact_names = ["大佬1", "大佬2", "大佬3", "大佬4"]
+                Preconditions.create_he_contacts(contact_names)
+                contact_names2 = [("b测算", "13800137001"), ("c平5", "13800137002"), ('哈 马上', "13800137003"),
+                                  ('陈丹丹', "13800137004"), ('alice', "13800137005"), ('郑海', "13802883296")]
+                Preconditions.create_he_contacts2(contact_names2)
+                flag2 = True
+            except:
+                fail_time2 += 1
+            if flag2:
+                break
+
+        Preconditions.make_already_in_message_page()
+        # 点击‘通讯录’
+        mess = MessagePage()
+        mess.open_contacts_page()
+        # 1、点击通讯录，点击搜索输入框
+        # 2、输入特殊字符
+        mess.click_search()
+        # 查询页面输入'大佬1'
+        search_page = SearchPage()
+        search_page.input_search_keyword('大佬1')
+        time.sleep(5)
+        search_page.hide_keyboard()
+        # 判定点
+        # 1.模糊匹配到正确的结果
+        self.assertEquals(search_page.is_text_present("查看更多"), False)
+
+    @tags('ALL', 'CONTACT', 'YL')
+    def test_contacts_chenjixiang_0740(self):
+        # 导入团队联系人
+        fail_time2 = 0
+        flag2 = False
+        while fail_time2 < 5:
+            try:
+                Preconditions.make_already_in_message_page()
+                contact_names = ["大佬1", "大佬2", "大佬3", "大佬4"]
+                Preconditions.create_he_contacts(contact_names)
+                contact_names2 = [("b测算", "13800137001"), ("c平5", "13800137002"), ('哈 马上', "13800137003"),
+                                  ('陈丹丹', "13800137004"), ('alice', "13800137005"), ('郑海', "13802883296")]
+                Preconditions.create_he_contacts2(contact_names2)
+                flag2 = True
+            except:
+                fail_time2 += 1
+            if flag2:
+                break
+
+        Preconditions.make_already_in_message_page()
+        # 点击‘通讯录’
+        mess = MessagePage()
+        mess.open_contacts_page()
+        # 1、点击通讯录，点击搜索输入框
+        # 2、输入特殊字符
+        mess.click_search()
+        # 查询页面输入'大佬1'
+        search_page = SearchPage()
+        search_page.input_search_keyword('大佬')
+        time.sleep(5)
+        search_page.hide_keyboard()
+        # 判定点
+        # 1.模糊匹配到正确的结果
+        self.assertEquals(search_page.is_text_present("查看更多"), True)
+
+    @tags('ALL', 'CONTACT', 'YL')
     def test_contacts_chenjixiang_0741(self):
         """查看更多联系人"""
         Preconditions.make_already_in_message_page()
@@ -102,23 +227,21 @@ class ContactSearchOpTest(TestCase):
         search_page.input_search_keyword('通讯录小于')
         time.sleep(5)
         search_page.hide_keyboard()
-        # 判定点
         # 1.展示群聊搜索结果标签，标签右上角不展示查看更多按钮
         self.assertEquals(search_page.is_text_present("查看更多"), False)
 
-        @tags('ALL', 'SMOKE')
-        def test_contacts_chenjixiang_0742(self):
-            Preconditions.make_already_in_message_page()
-            # 点击‘通讯录’
-            mess = MessagePage()
-            mess.open_contacts_page()
-            mess.click_search()
-            # 查询页面输入'通讯录小于'
-            search_page = SearchPage()
-            search_page.input_search_keyword('群')
-            time.sleep(5)
-            search_page.hide_keyboard()
-            # 判定点
-            # 1.展示群聊搜索结果标签，标签右上角展示查看更多按钮  群聊 & 查看更多
-            self.assertEquals(search_page.is_text_present("查看更多"), True)
+    @tags('ALL', 'CONTACT', 'YL')
+    def test_contacts_chenjixiang_0742(self):
+        Preconditions.make_already_in_message_page()
+        # 点击‘通讯录’
+        mess = MessagePage()
+        mess.open_contacts_page()
+        mess.click_search()
+        # 查询页面输入'通讯录小于'
+        search_page = SearchPage()
+        search_page.input_search_keyword('群')
+        time.sleep(5)
+        search_page.hide_keyboard()
+        # 1.展示群聊搜索结果标签，标签右上角展示查看更多按钮  群聊 & 查看更多
+        self.assertEquals(search_page.is_text_present("查看更多"), True)
 
