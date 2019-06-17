46dc7699022dcd8d08fa6e0d73fb0beaf7c7cb98
dengbenhai <dengbenhai@chinasoftinc.com>
2019-06-17 21:12:31 +0800

    编写消息模块用例 msg_huangcaizui_D_0048

--- TestCase/m007_others/other_workbench/workbench_all.py
+++ TestCase/m007_others/other_workbench/workbench_all.py
@@ -2100,6 +2100,33 @@ class MsgAllPrior(TestCase):
                                                      '//*[@resource-id="com.chinasofti.rcs:id/textview_file_name"]'))
         self.assertTrue(len(page_elements) > 0)
 
+    @staticmethod
+    def setUp_test_msg_huangcaizui_D_0048():
+        Preconditions.select_mobile('Android-移动')
+        # 启动后不论当前在哪个页面，强制进入消息页面
+        Preconditions.force_enter_message_page_631()
+
+    @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
+    def test_msg_huangcaizui_D_0048(self):
+        """1、点击GIF"""
+        mess = MessagePage()
+        # 判断网络是否正常
+        ns = mess.get_network_status()
+        self.assertTrue(ns in [2, 4, 6])
+        # 点击我的电脑
+        self.assertTrue(mess.page_should_contain_my_computer())
+        mess.click_my_computer()
+        cwp = ChatWindowPage()
+        # 点击表情
+        cwp.click_expression()
+        time.sleep(1)
+        # 点击gif
+        cwp.click_gif()
+        cgp = ChatGIFPage()
+        cgp.wait_for_page_load()
+        # 判断是否出现gif界面
+        self.assertTrue(cgp.is_on_this_page())
+
 
 class Contacts_demo(TestCase):
 
