44f355aaa405852f8fb4584e33f8d929c25acafa
huangqin <huangqin002@chinasoftinc.com>
2019-06-17 21:17:59 +0800

    编写消息模块用例2条 msg_huangcaizui_D_0053 0054

--- TestCase/m007_others/other_workbench/workbench_all.py
+++ TestCase/m007_others/other_workbench/workbench_all.py
@@ -2222,6 +2222,69 @@ class MsgAllPrior(TestCase):
         # 判断是否发送成功
         cwp.wait_for_msg_send_status_become_to("发送成功", max_wait_time=10)
 
+    @staticmethod
+    def setUp_test_msg_huangcaizui_D_0053():
+        Preconditions.select_mobile('Android-移动')
+        # 启动后不论当前在哪个页面，强制进入消息页面
+        Preconditions.force_enter_message_page_631()
+
+    @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
+    def test_msg_huangcaizui_D_0053(self):
+        """1、点击GIF图标 2、搜索框输入特殊字符 3、点击选择表情"""
+        # 判断网络是否正常
+        mess = MessagePage()
+        ns = mess.get_network_status()
+        self.assertTrue(ns in [2, 4, 6])
+        # 点击我的电脑
+        self.assertTrue(mess.page_should_contain_my_computer())
+        mess.click_my_computer()
+        cwp = ChatWindowPage()
+        # 点击表情
+        cwp.click_expression()
+        time.sleep(1)
+        # 点击gif
+        cwp.click_gif()
+        cgp = ChatGIFPage()
+        cgp.wait_for_page_load()
+        # 点击表情搜搜
+        cgp.input_message("q")
+        self.assertTrue(cwp.is_toast_exist("无搜索结果，换个热词试试"))
+
+    @staticmethod
+    def setUp_test_msg_huangcaizui_D_0054():
+        Preconditions.select_mobile('Android-移动')
+        # 启动后不论当前在哪个页面，强制进入消息页面
+        Preconditions.force_enter_message_page_631()
+
+    @tags('ALL', 'SMOKE', 'CMCC', 'group_chat', 'prior', 'high')
+    def test_msg_huangcaizui_D_0054(self):
+        """1、点击GIF图标 2、搜索框输入关键字匹配到对应结果后点击返回 3、再次进入该会话页面"""
+        # 判断网络是否正常
+        mess = MessagePage()
+        ns = mess.get_network_status()
+        self.assertTrue(ns in [2, 4, 6])
+        # 点击我的电脑
+        self.assertTrue(mess.page_should_contain_my_computer())
+        mess.click_my_computer()
+        cwp = ChatWindowPage()
+        # 点击表情
+        cwp.click_expression()
+        time.sleep(1)
+        # 点击gif
+        cwp.click_gif()
+        cgp = ChatGIFPage()
+        cgp.wait_for_page_load()
+        # 点击表情搜搜
+        cgp.input_message("6")
+        time.sleep(1)
+        # 退出对话
+        cwp.click_back1()
+        time.sleep(1)
+        # 再次点击我的电脑
+        mess.click_my_computer()
+        # 判断搜索结果是否存在
+        self.assertFalse(cgp.is_on_this_page())
+
 
 class Contacts_demo(TestCase):
 
