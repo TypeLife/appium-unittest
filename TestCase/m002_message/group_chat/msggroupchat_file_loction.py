3799c97de3ae67629dd866175f337feb4cff336e
liwenxiu <liwenxiu01@chinasoftinc.com>
2019-06-26 20:49:26 +0800

    编写消息模块用例： msg_weifenglian_qun_0355

--- TestCase/m002_message/group_chat/msggroupchat_file_loction.py
+++ TestCase/m002_message/group_chat/msggroupchat_file_loction.py
@@ -2795,6 +2795,33 @@ class MsgGroupChatFileLocationTest(TestCase):
         if not gcp.is_on_this_page():
             raise AssertionError("当前页面不在群聊页面")
 
+    @tags('ALL', 'CMCC', 'group_chat', 'full', 'high', 'yx')
+    def test_msg_weifenglian_qun_0355(self):
+        """将自己发送的位置转发到在企业列表搜索框输入数字搜索到的团队联系人"""
+        Preconditions.public_send_location()
+        # 1.长按位置消息体转发
+        gcp = GroupChatPage()
+        gcp.press_message_to_do("转发")
+        scp = SelectContactsPage()
+        scp.wait_for_page_load()
+        # 2.点击“选择和通讯录联系人”菜单
+        scp.click_he_contacts()
+        shc = SelectHeContactsDetailPage()
+        shc.wait_for_he_contacts_page_load()
+        # 3.在搜索框输入数字
+        shc.input_search("1122")
+        # 4.点击搜索的团队联系人
+        shc.click_search_team_contacts()
+        # 5.点击确认转发
+        shc.click_sure_forward()
+        flag = gcp.is_toast_exist("已转发")
+        if not flag:
+            raise AssertionError("在转发发送自己的位置时，没有‘已转发’提示")
+        if not gcp.is_on_this_page():
+            raise AssertionError("当前页面不在群聊页面")
+
+
+
 
 
 
