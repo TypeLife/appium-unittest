0b0d2f48593f91a8b0ca187c4f3182402e57da68
liwenxiu <liwenxiu01@chinasoftinc.com>
2019-07-01 19:25:17 +0800

    编写消息模块用例： msg_weifenglian_qun_0375

--- TestCase/m002_message/group_chat/msggroupchat_file_loction.py
+++ TestCase/m002_message/group_chat/msggroupchat_file_loction.py
@@ -3720,5 +3720,49 @@ class MsgGroupChatFileLocationTest(TestCase):
         mess = MessagePage()
         mess.set_network_status(6)
 
+    @tags('ALL', 'CMCC', 'group_chat', 'full', 'high', 'yx')
+    def test_msg_weifenglian_qun_0075(self):
+        """将自己发送的文件转发到企业群"""
+        gcp = GroupChatPage()
+        # 1.点击文件
+        gcp.click_file()
+        csf = ChatSelectFilePage()
+        csf.wait_for_page_load()
+        # 2.点击本地文件
+        csf.click_local_file()
+        # 3.选择任意文件，点击发送按钮
+        local_file = ChatSelectLocalFilePage()
+        # 4.进入预置文件目录，选择文件发送
+        local_file.push_preset_file()
+        local_file.click_preset_file_dir()
+        # 5.点击选择发送文件
+        local_file.select_file('.txt')
+        local_file.click_send()
+        # 6.长按最后一个文件转发
+        gcp.press_last_file_to_do("转发")
+        scp = SelectContactsPage()
+        scp.wait_for_page_load()
+        # 7.点击选择一个企业群
+        scp.click_select_one_group()
+        sogp = SelectOneGroupPage()
+        sogp.wait_for_page_load()
+        names = sogp.get_group_name()
+        firm_names = []
+        for name in names:
+            if '企业' in name:
+                firm_names.append(name)
+        firm_name = random.choice(firm_names)
+        if firm_name:
+            sogp.select_one_group_by_name(firm_name)
+            # 8、点击确定
+            sogp.click_sure_forward()
+            flag = gcp.is_toast_exist("已转发")
+            if not flag:
+                raise AssertionError("在转发发送自己的位置时，没有‘已转发’提示")
+            if not gcp.is_on_this_page():
+                raise AssertionError("当前页面不在群聊天会话页面")
+        else:
+            raise AssertionError("需要创建企业群")
+
 
 
